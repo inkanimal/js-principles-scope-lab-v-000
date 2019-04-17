96f63b762c0e05d2571345776d06d2dc48037f7a
geoffrey woodcock <theatomproject@gmail.com>
2019-04-17 16:10:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var customerName = 'bob';
 
 
-function upperCaseCustomerName() {
+function upperCaseCustomerName(customerName) {
   
-   upperCustomerName = customerName.toUpperCase();
+  customerName.toUpperCase();
 }

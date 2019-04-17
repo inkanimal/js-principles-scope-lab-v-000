f51b258082a9392bdd91664a8ef1b1b76dc8b336
geoffrey woodcock <theatomproject@gmail.com>
2019-04-17 16:01:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 // Write your solution in this file!
 var customerName = 'bob';
 
-function upperCaseCustomerName(customerName) {
-  customerName.toUpperCase();
-  return customerName;
-}
+
 function upperCaseCustomerName() {
   cunstomerName = "bob";
    upperCustomerName = customerName.toUpperCase();
